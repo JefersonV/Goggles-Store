6f9ff92dbc1e7c53f5579b9c3e443dede7f596e8
JefersonV <jvelasquezc15@miumg.edu.gt>
2021-02-24 01:21:30 -0600

    refactor a los estilos de sass

---
+++ src/js/card.js
@@ -0,0 +1,10 @@
+let image = document.querySelector('.product-image');
+console.log(image);
+let link = document.querySelector('.link-product-add-cart')
+console.log(link)
+
+function showBtn() {
+    link.classList.toggle('cart-active')
+}
+
+image.addEventListener("click", showBtn);
